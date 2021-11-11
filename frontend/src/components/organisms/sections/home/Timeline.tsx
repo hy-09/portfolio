df2a29a351fe3481a2d3cd419ac605c7a16ad886
hy-09 <harada_yuki@icloud.com>
2021-11-11 16:22:58 +0900

    いいねした投稿一覧

--- frontend/src/components/organisms/sections/home/Timeline.tsx
+++ frontend/src/components/organisms/sections/home/Timeline.tsx
@@ -1,5 +1,5 @@
 import { AppBar, Grid, makeStyles, Tab, Tabs } from "@material-ui/core"
-import { Favorite, PersonPinCircleOutlined } from "@material-ui/icons"
+import { Favorite, Person, PersonPinCircleOutlined } from "@material-ui/icons"
 import { ChangeEvent, FC, useState } from "react"
 import { useAppSelector } from "../../../../app/hooks"
 import Title from "../../../atoms/Title"
@@ -8,7 +8,9 @@ import Section from "../../Section"
 
 const Timeline: FC = () => {
     const [value, setValue] = useState(0);
+    const loginUser = useAppSelector(state => state.auth.loginUser)
     const myPosts = useAppSelector(state => state.post.myPosts)
+    const likePosts = useAppSelector(state => state.post.posts).filter(post => post.likeUsers.includes(loginUser.id))
 
     const handleChange = (event: ChangeEvent<{}>, newValue: number) => {
         setValue(newValue);
@@ -30,7 +32,7 @@ const Timeline: FC = () => {
                             onChange={handleChange} 
                             aria-label="simple tabs example"
                         >
-                            <Tab icon={<PersonPinCircleOutlined />} aria-label="person" {...a11yProps(0)} />
+                            <Tab icon={<Person />} aria-label="person" {...a11yProps(0)} />
                             <Tab icon={<Favorite />} aria-label="favorite" {...a11yProps(1)} />
                         </Tabs>
                     </AppBar>
@@ -38,7 +40,7 @@ const Timeline: FC = () => {
                         <Posts allPosts={myPosts} />
                     )}
                     {value == 1 && (
-                        <Posts allPosts={[]} />
+                        <Posts allPosts={likePosts} />
                     )}
                 </div>
             </Section>
